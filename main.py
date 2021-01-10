ca245ddb7dc5dbf5e8bcbefea53b5868f876050d
yoshi <y.oonishi.1020@gmail.com>
2021-01-10 12:31:07 +0900

    項目を選択可能

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import pandas as pd
 
 st.title('RINQWARE 決算公告')
 "損益計算書(P/L)"
-pl = pd.read_csv('csv/pl.csv', index_col=0)
-st.line_chart(pl)
+pl_df = pd.read_csv('csv/pl.csv', index_col=0)
+
+options = st.multiselect(
+    "表示する項目を選択してください。",
+    list(pl_df.columns),
+    default=list(pl_df.columns),
+    )
+
+if options:
+   st.line_chart(pl_df[options])
